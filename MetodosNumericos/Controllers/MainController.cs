4e84fbe49c3a73d6275aaad276ea928b2bbc0420
sjg99 <38005341+sjg99@users.noreply.github.com>
2018-08-14 13:01:15 -0500

    Initial Commit
    xd

---
+++ MetodosNumericos/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using MetodosNumericos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetodosNumericos.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetInData(MainLoading ml)
+        {
+
+            return View();
+        }
+    }
+}
